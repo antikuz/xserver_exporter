7b39112f409ffa9fff5b6a3dd18bc53b926f9caa
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-06-04 12:14:28 +0300

    add prometheus collector

---
+++ collector/collector.go
@@ -0,0 +1,121 @@
+package collector
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type xserverCollector struct {
+	xserver
+}
+
+type xserver struct {
+	url    string
+	login  string
+	passwd string
+	insecureSkip bool
+}
+
+type Envelope struct {
+	Oid string `json:"oid"`
+}
+
+func (xserver xserver) getJson() ([]byte, error) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: xserver.insecureSkip,
+			},
+		},
+	}
+
+	request, err := http.NewRequest("GET", xserver.url, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	auth := base64.StdEncoding.EncodeToString([]byte(
+		fmt.Sprintf("%s:%s", xserver.login, xserver.passwd)))
+	authorizationHeader := fmt.Sprintf("Basic %s", auth)
+	
+	request.Header.Add("content-type", "application/json")
+	request.Header.Add("authorization", authorizationHeader)
+	
+	res, err := client.Do(request)
+	if err != nil {
+        log.Fatal(err)
+    }
+	defer res.Body.Close()
+	
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+        return nil, err
+    }
+
+    return body, nil
+}
+
+func (xserver xserver) parseJson(arrayJsonRaw []*json.RawMessage) {
+    var env Envelope
+    for _, element := range arrayJsonRaw {
+        if err := json.Unmarshal(*element, &env); err != nil {
+            log.Fatal(err)
+        }
+        switch env.Oid {
+        case "UsersWidget":
+            if err := json.Unmarshal(*element, &UsersWidget); err != nil {
+                log.Fatal(err)
+            }
+        case "StatWidget":
+            if err := json.Unmarshal(*element, &Netstat); err != nil {
+                log.Fatal(err)
+            }
+        case "IfacesWidget":
+            if err := json.Unmarshal(*element, &Netstat); err != nil {
+                log.Fatal(err)
+            }
+        case "MonitoringWidget":
+            if err := json.Unmarshal(*element, &MonitoringWidget); err != nil {
+                log.Fatal(err)
+            }
+        }
+    }
+}
+
+func (xserver xserverCollector) Describe(ch chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(xserver, ch)
+}
+
+func (xserver xserverCollector) Collect(ch chan<- prometheus.Metric) {
+    request, err := xserver.getJson()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var arrayJsonRaw []*json.RawMessage
+    json.Unmarshal(request, &arrayJsonRaw)
+	xserver.parseJson(arrayJsonRaw)
+
+	UsersWidget.Collect(ch)
+	Netstat.Collect(ch)
+	MonitoringWidget.Collect(ch)
+}
+
+func NewXserverManager(url string, login string, passwd string, insecureSkip bool, reg prometheus.Registerer) *xserverCollector {
+	x := xserver{
+			url: url,
+			login: login,
+			passwd: passwd,
+			insecureSkip: insecureSkip,
+		}
+	xc := &xserverCollector{x}
+	prometheus.WrapRegistererWith(prometheus.Labels{"url": url}, reg).MustRegister(xc)
+	return xc
+}
--- main.go
+++ main.go
@@ -1,130 +1,28 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/antikuz/xserver_exporter/collector"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type UsersWidget struct {
-	Active  int    `json:"active,omitempty"`
-	Blocked int    `json:"blocked,omitempty"`
-	Enabled int    `json:"enabled,omitempty"`
-	Users   int    `json:"users,omitempty"`
-	Vpn     int    `json:"vpn,omitempty"`
-}
-
-type StatWidget struct {
-	Stat  map[string]int `json:"stat,omitempty"`
-}
-
-type IfacesWidget struct {
-	Today struct {
-		Ping           string `json:"ping,omitempty"`
-		VpnConnections int    `json:"vpn_connections,omitempty"`
-		ChannelUsage   struct {
-			Rp float32 `json:"rp,omitempty"`
-			Tp float32 `json:"tp,omitempty"`
-			Rx float32 `json:"rx,omitempty"`
-			Tx float32 `json:"tx,omitempty"`
-		} `json:"channelUsage,omitempty"`
-	} `json:"today,omitempty"`
-}
-
-type MonitoringWidget struct {
-	Cpu   int     `json:"cpu,omitempty"`
-	Ram   int     `json:"ram,omitempty"`
-	Swap  int     `json:"swap,omitempty"`
-	MaxLa int     `json:"maxLa,omitempty"`
-	La    float32 `json:"la,omitempty"`
-}
-
-type Envelope struct {
-	Oid string `json:"oid"`
-}
-
-var (
-    usersWidget UsersWidget
-    statWidget StatWidget
-    ifacesWidget IfacesWidget
-    monitoringWidget MonitoringWidget
-)
-
-func parseJson(arrayJsonRaw []*json.RawMessage) {
-    var env Envelope
-    for _, element := range arrayJsonRaw {
-        if err := json.Unmarshal(*element, &env); err != nil {
-            log.Fatal(err)
-        }
-        switch env.Oid {
-        case "UsersWidget":
-            if err := json.Unmarshal(*element, &usersWidget); err != nil {
-                log.Fatal(err)
-            }
-        case "StatWidget":
-            if err := json.Unmarshal(*element, &statWidget); err != nil {
-                log.Fatal(err)
-            }
-        case "IfacesWidget":
-            if err := json.Unmarshal(*element, &ifacesWidget); err != nil {
-                log.Fatal(err)
-            }
-        case "MonitoringWidget":
-            if err := json.Unmarshal(*element, &monitoringWidget); err != nil {
-                log.Fatal(err)
-            }
-        }
-    }
-}
-
-func getJson() []byte {
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
-
+func main() {
 	url := "http://localhost:8000"
-
-	request, err := http.NewRequest("GET", url, nil)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
 	login := "login"
 	passwd := "passwd"
-	auth := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", login, passwd)))
-	authorizationHeader := fmt.Sprintf("Basic %s", auth)
-	
-	request.Header.Add("content-type", "application/json")
-	request.Header.Add("authorization", authorizationHeader)
-	res, err := client.Do(request)
-	if err != nil {
-        log.Fatal(err)
-    }
+	insecureSkip := true
 
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-        log.Fatal(err)
-    }
-    return body
-}
-
-func main() {
 	reg := prometheus.NewPedanticRegistry()
-	NewClusterManager("db", reg)
+	collector.NewXserverManager(
+		url,
+		login,
+		passwd,
+		insecureSkip,
+		reg)
 
 	reg.MustRegister(
 		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
