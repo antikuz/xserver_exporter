2b9bca491012520737f3eb25f09e2f818af6640b
Antikuz <36120950+Antikuz@users.noreply.github.com>
2022-06-04 10:33:19 +0300

    add network statistic in netstat

--- collector/netstat.go
+++ collector/netstat.go
@@ -7,6 +7,12 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+
+type netstat struct {
+	IfacesWidget
+	StatWidget
+}
+
 type IfacesWidget struct {
 	Today `json:"today"`
 }
@@ -14,7 +20,7 @@ type IfacesWidget struct {
 type Today struct {
 	Ping           string `json:"ping"`
 	VpnConnections int    `json:"vpn_connections"`
-	ChannelUsage `json:"channelUsage"`
+	ChannelUsage   `json:"channelUsage"`
 }
 
 type ChannelUsage struct {
@@ -24,7 +30,25 @@ type ChannelUsage struct {
 	Tx float64 `json:"tx"`
 }
 
+type StatWidget struct {
+	Stat `json:"stat"`
+}
+
+type Stat struct {
+	RxMonth float64 `json:"rxMonth"`
+	TxMonth float64 `json:"txMonth"`
+	RxWeek  float64 `json:"rxWeek"`
+	TxWeek  float64 `json:"txWeek"`
+	RxDay   float64 `json:"rxDay"`
+	TxDay   float64 `json:"txDay"`
+}
+
 var (
+	Netstat = netstat{
+		IfacesWidget{},
+		StatWidget{},
+	}
+
 	netstatPingDesc = prometheus.NewDesc(
 		"xserver_netstat_ping",
 		"Ping to ya.ru",
@@ -55,10 +79,40 @@ var (
 		"Transmit bytes per second",
 		nil, nil,
 	)
+	netstatStatRxMonthDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_rx_month",
+		"Received bytes per month",
+		nil, nil,
+	)
+	netstatStatTxMonthDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_tx_month",
+		"Transmit bytes per month",
+		nil, nil,
+	)
+	netstatStatRxWeekDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_rx_week",
+		"Received bytes per week",
+		nil, nil,
+	)
+	netstatStatTxWeekDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_tx_week",
+		"Transmit bytes per week",
+		nil, nil,
+	)
+	netstatStatRxDayDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_rx_day",
+		"Received bytes per day",
+		nil, nil,
+	)
+	netstatStatTxDayDesc = prometheus.NewDesc(
+		"xserver_netstat_stat_tx_day",
+		"Transmit bytes per day",
+		nil, nil,
+	)
 )
 
-func (i IfacesWidget) Collect(ch chan<- prometheus.Metric) {
-	ping, err := strconv.ParseFloat(i.Ping, 64)
+func (n netstat) Collect(ch chan<- prometheus.Metric) {
+	ping, err := strconv.ParseFloat(n.Ping, 64)
 	if err != nil {
         log.Fatal(err)
     }
@@ -68,22 +122,46 @@ func (i IfacesWidget) Collect(ch chan<- prometheus.Metric) {
 	)
 
 	ch <- prometheus.MustNewConstMetric(
-		netstatVpnConnectionsDesc, prometheus.GaugeValue, float64(i.VpnConnections),
+		netstatVpnConnectionsDesc, prometheus.GaugeValue, float64(n.VpnConnections),
 	)
 
 	ch <- prometheus.MustNewConstMetric(
-		netstatChannelUsageRpDesc, prometheus.GaugeValue, i.Rp,
+		netstatChannelUsageRpDesc, prometheus.GaugeValue, n.Rp,
 	)
 
 	ch <- prometheus.MustNewConstMetric(
-		netstatChannelUsageTpDesc, prometheus.GaugeValue, i.Tp,
+		netstatChannelUsageTpDesc, prometheus.GaugeValue, n.Tp,
 	)
 
 	ch <- prometheus.MustNewConstMetric(
-		netstatChannelUsageRxDesc, prometheus.GaugeValue, i.Rx,
+		netstatChannelUsageRxDesc, prometheus.GaugeValue, n.Rx,
 	)
 
 	ch <- prometheus.MustNewConstMetric(
-		netstatChannelUsageTxDesc, prometheus.GaugeValue, i.Tx,
+		netstatChannelUsageTxDesc, prometheus.GaugeValue, n.Tx,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatRxMonthDesc, prometheus.GaugeValue, n.RxMonth,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatTxMonthDesc, prometheus.GaugeValue, n.TxMonth,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatRxWeekDesc, prometheus.GaugeValue, n.RxWeek,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatTxWeekDesc, prometheus.GaugeValue, n.TxWeek,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatRxDayDesc, prometheus.GaugeValue, n.RxDay,
+	)
+
+	ch <- prometheus.MustNewConstMetric(
+		netstatStatTxDayDesc, prometheus.GaugeValue, n.TxDay,
 	)
 }
